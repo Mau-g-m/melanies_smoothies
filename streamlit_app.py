d6681f405b48c90eaa4babde6e1be1d6dc4c8c42
Mau-g-m <mauricio.ga.ma@outlook.com>
2025-04-08 18:00:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,5 @@ if submit:
             st.error(f"Something went wrong: {e}")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+# st.text(smoothiefroot_response.json)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
